e46f0e8e370f31a135740ff02939ef98aa324649
Lucas França Marinho <lucasfrancamarinho16@gmail.com>
2023-04-03 16:59:18 -0300

    Add New testimonials

--- js/main.js
+++ js/main.js
@@ -250,16 +250,16 @@
     slider();
 })();
 
-(() =>{
+// (() =>{
 
-    const sections = document.querySelectorAll(".section");
-    sections.forEach((section) =>{
-        if(!section.classList.contains("active")){
-            section.classList.add("hide");
-        }
-    })
+//     const sections = document.querySelectorAll(".section");
+//     sections.forEach((section) =>{
+//         if(!section.classList.contains("active")){
+//             section.classList.add("hide");
+//         }
+//     })
 
-})();
+// })();
 
 window.addEventListener("load", () =>{
     document.querySelector(".preloader").classList.add("fade-out");
