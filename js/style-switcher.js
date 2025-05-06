7e6eba37337b41ee7f0118f2da6623a44fb715f5
Lucas França Marinho <lucasfrancamarinho16@gmail.com>
2025-05-06 19:41:38 -0300

    Atualizando dados

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,41 +1,36 @@
 const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
 
-styleSwitcherToggler.addEventListener("click", () =>{
+styleSwitcherToggler.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
-})
+});
 
-window.addEventListener("scroll", () =>{
-    if(document.querySelector(".style-switcher").classList.contains("open")){
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
     }
-})
+});
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
 
-function setActiveStyle(color){
-    alternateStyles.forEach((style) =>{
-        if(color === style.getAttribute("title")){
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
             style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
         }
-        else{
-            style.setAttribute("disabled","true");
-        }
-    })
+    });
 }
 
 const dayNight = document.querySelector(".day-night");
 
-    dayNight.addEventListener("click", () =>{
-        dayNight.querySelector("i").classList.toggle("fa-sun");
-        dayNight.querySelector("i").classList.toggle("fa-moon");
-        document.body.classList.toggle("dark");
-    })
+window.addEventListener("load", () => {
+    document.body.classList.add("dark");
+    dayNight.querySelector("i").classList.add("fa-sun");
+});
 
-window.addEventListener("load", () =>{
-    if(document.body.classList.contains("dark")){
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else{
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
-})
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
